f4bc92c7aa6d37434f3deac85fc437f6e25d83ed
Aaron Mansfield <37600872+aaronmansfield5@users.noreply.github.com>
2023-04-02 20:09:11 +0100

    Update sanitize.js

--- sanitize.js
+++ sanitize.js
@@ -1,11 +1,6 @@
-/**
- * The SQL class provides methods for sanitizing SQL queries to help prevent SQL injection attacks.
- * It's important to note that this class is not a comprehensive solution for securing your database
- * and should be used in conjunction with other security measures, such as prepared statements.
- */
 class SQL {
     constructor() {
-        this.restrictedKeywords = [
+        this.restrictedKeywords = new Set([
             "SELECT",
             "INSERT",
             "UPDATE",
@@ -18,110 +13,52 @@ class SQL {
             "UNION",
             "GRANT",
             "REVOKE",
-        ];
+        ]);
 
         this.restrictedKeywordsPattern = this._buildRestrictedKeywordsPattern();
     }
 
-    /**
-     * Sanitize the values within an SQL query to prevent SQL injection.
-     *
-     * @param {string} query - The SQL query to sanitize.
-     * @returns {string} - The sanitized SQL query.
-     */
     sanitize(query) {
         if (typeof query !== "string") throw new Error("Query must be a string.");
-
-        // Escape single quotes (') within the query
-        const escapedQuery = this._escapeSingleQuotes(query);
-
-        // Remove potentially dangerous keywords from the query
-        const sanitizedQuery = this._removeRestrictedKeywords(escapedQuery);
-
-        return sanitizedQuery;
+        return this._sanitizeQuery(query);
     }
 
-    /**
-     * Escape single quotes within the query by replacing them with two single quotes.
-     *
-     * @param {string} query - The SQL query to escape.
-     * @returns {string} - The query with escaped single quotes.
-     * @private
-     */
-    _escapeSingleQuotes(query) {
-        const singleQuotePattern = /'/g;
-        return query.replace(singleQuotePattern, "''");
+    _sanitizeQuery(query) {
+        // Replace single quotes and match restricted keywords at the same time
+        return query.replace(/'|(\b(?:SELECT|INSERT|UPDATE|DELETE|DROP|ALTER|CREATE|TRUNCATE|EXEC|UNION|GRANT|REVOKE)\b)/gi, (match, keyword) => {
+            if (match === "'") {
+                return "''";
+            } else if (this.restrictedKeywords.has(keyword.toUpperCase())) {
+                return "";
+            } else {
+                return match;
+            }
+        });
     }
 
-    /**
-     * Remove potentially dangerous SQL keywords from the query.
-     *
-     * @param {string} query - The SQL query to sanitize.
-     * @returns {string} - The sanitized SQL query with restricted keywords removed.
-     * @private
-     */
-    _removeRestrictedKeywords(query) {
-        return query.replace(this.restrictedKeywordsPattern, "");
-    }
-
-    /**
-     * Add a restricted keyword to the list of keywords that should be removed from queries.
-     *
-     * @param {string} keyword - The restricted keyword to add.
-     * @returns {void}
-     */
     addRestrictedKeyword(keyword) {
         if (typeof keyword !== "string") throw new Error("Keyword must be a string.");
-        if (this.restrictedKeywords.includes(keyword.toUpperCase())) return;
+        if (this.restrictedKeywords.has(keyword.toUpperCase())) return;
 
-        this.restrictedKeywords.push(keyword.toUpperCase());
-        this._updateRestrictedKeywordsPattern();
-    }
-
-    /**
-     * Remove a restricted keyword from the list of keywords that should be removed from queries.
-     *
-     * @param {string} keyword - The restricted keyword to remove.
-     * @returns {void}
-     */
-    removeRestrictedKeyword(keyword) {
-        if (typeof keyword !== "string") throw new Error("Keyword must be a string.");
-        const index = this.restrictedKeywords.indexOf(keyword.toUpperCase());
-        if (index === -1) return;
-
-        this.restrictedKeywords.splice(index, 1);
-        this._updateRestrictedKeywordsPattern();
-    }
-
-    /**
-     * Update the restricted keywords pattern based on the current list of restricted keywords.
-     *
-     * @returns {void}
-     * @private
-     */
-    _updateRestrictedKeywordsPattern() {
+        this.restrictedKeywords.add(keyword.toUpperCase());
         this.restrictedKeywordsPattern = this._buildRestrictedKeywordsPattern();
     }
 
-    /**
-     * Build the RegExp pattern for the current list of restricted keywords.
-     *
-     * @returns {RegExp} - The restricted keywords pattern.
-     * @private
-     */
+    removeRestrictedKeyword(keyword) {
+        if (typeof keyword !== "string") throw new Error("Keyword must be a string.");
+        if (!this.restrictedKeywords.has(keyword.toUpperCase())) return;
+
+        this.restrictedKeywords.delete(keyword.toUpperCase());
+        this.restrictedKeywordsPattern = this._buildRestrictedKeywordsPattern();
+    }
+
     _buildRestrictedKeywordsPattern() {
         return new RegExp(
-            `\\b(?:${this.restrictedKeywords.join("|")})\\b`,
+            `\\b(?:${Array.from(this.restrictedKeywords).join("|")})\\b`,
             "gi"
         );
     }
 
-    /**
-     * Check if the provided keyword is valid.
-     *
-     * @param {string} keyword - The keyword to validate.
-     * @returns {boolean} - Returns true if the keyword is valid, false otherwise.
-     */
     isValidKeyword(keyword) {
         if (typeof keyword !== "string") return false;
 
